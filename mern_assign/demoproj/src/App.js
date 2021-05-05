22f849b602acf3ea00e12a2012b91fbf591bdbdb
Julian Martinez <julianmartinez1830@gmail.com>
2021-05-05 12:35:19 -0500

    First JSX

---
+++ mern_assign/demoproj/src/App.js
@@ -0,0 +1,25 @@
+// import logo from './logo.svg';
+// import './App.css';
+// import React from `react`; -is this needed?
+
+//  this is a component
+function App() {
+  return (
+    <>
+    <div className="App">
+      <h1>HelloWorld</h1>
+    </div>
+    <div>
+      <h3>Things I Need To Do:</h3>
+      <ol>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ol>
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ mern_assign/demoproj/src/components/test.jsx
@@ -0,0 +1,10 @@
+class App extends Component{
+    render(){
+        return (
+            <>
+                <h1>Hello World</h1>
+                <h3 className="my-class">This is JSX</h3>
+            </>
+        );
+    }
+}
