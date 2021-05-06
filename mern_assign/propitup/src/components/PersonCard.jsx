a739f96e611bbf3caa9d222d0b627d994fa0d844
Julian Martinez <julianmartinez1830@gmail.com>
2021-05-05 21:25:04 -0500

    PropItUp Assignment

---
+++ mern_assign/propitup/src/components/PersonCard.jsx
@@ -0,0 +1,24 @@
+// import React, { Component } from 'react';
+
+
+// class SomeClassComponent extends Component {
+//     render() {
+//         return <div>This is our first class component.</div>;
+//     }
+// }
+
+// export default SomeClassComponent;
+import React, {Component} from 'react';
+
+class PersonCard extends Component{
+    render(){
+        
+        return <div className="cardName">
+            <h1>{this.props.firstName} {this.props.lastName}</h1>
+            <p>Age: {this.props.age}</p>
+            <p>Hair Color: {this.props.hairColor}</p>
+        </div>
+    }
+}
+
+export default PersonCard;
