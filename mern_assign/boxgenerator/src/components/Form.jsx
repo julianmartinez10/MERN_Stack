4fb73f42557d71aa8f1b77e957b57bd4a3e7611f
Julian Martinez <julianmartinez1830@gmail.com>
2021-05-12 21:28:33 -0500

    Box Generator minus 1 ninja bonus

---
+++ mern_assign/boxgenerator/src/components/Form.jsx
@@ -0,0 +1,71 @@
+import React, {useState} from 'react';
+import styles from './FormComponent.module.css'
+
+
+const Form = () => {
+
+    //  state variable to store the form information
+    const [formInfo, setFormInfo] = useState({
+        color:"",
+    });
+
+    //  state variable to store all the SUBMITTED form info
+    //  every time info is submitted it'll be pushed in to arr
+    const [allBoxes, setAllBoxes] = useState([])
+
+    //  each event listener should have a "handler"
+    //  changeHandler
+    const changeHandler = (e)=>{
+        console.log("You be typing in the form!!!")
+        console.log(e.target.name)
+        console.log(e.target.value)
+
+        //  setting form info
+        setFormInfo({
+            //  add a ...spread to make a copy and preserve other key:values
+            //  preserving key:value pairs with ...spread
+            ...formInfo,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    //  submitHandler
+    const submitHandler = (event)=>{
+        event.preventDefault()
+        console.log("Thanks for submitting your color suggestion!")
+        //  now I want the arr containing all form submissions (aka allBoxes) to add this new form info
+        setAllBoxes([...allBoxes, formInfo])
+
+        //  modify object means to setObject
+        setFormInfo({
+            color:""
+        })
+    }
+
+
+    return (
+        <div className="container">
+            <div>
+                <form onSubmit={submitHandler} className="col-4 offset-md-4 mt-4 form-inline">
+                    <div className="form-group mx-sm-3 mb-2">
+                        <label htmlFor="inputColor" className="mx-sm-2 text-capitalize text-info font-weight-bold">Color</label>
+                        <input onChange={changeHandler} type="text" className="form-control" id="inputColor" name="color" value={formInfo.color}/>
+                    </div>
+                    <button type="submit" className="btn btn-primary mb-2 offset-md-4">Submit Color</button>
+                </form>
+                <div >
+                    {
+                    allBoxes.map(box=>{
+                        return <div className={styles.boxcontainer} style = {{backgroundColor: box.color}}>
+                            <h1 className={styles.fonty}>{box.color}</h1>
+                        </div>
+                    })
+                    }
+                </div>
+            </div>
+        </div>
+    );
+};
+
+
+export default Form;
