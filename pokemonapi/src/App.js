7aa8eb3042fec7d45612d8f3f643ccc961b00588
Julian Martinez <julianmartinez1830@gmail.com>
2021-05-14 14:09:26 -0500

    Pokemon API

---
+++ pokemonapi/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import PokemonForm from './components/PokemonForm';
+
+function App() {
+  return (
+    <div className="App">
+      <PokemonForm></PokemonForm>
+    </div>
+  );
+}
+
+export default App;
---
+++ pokemonapi/src/components/PokemonForm.jsx
@@ -0,0 +1,57 @@
+import React, {useState, useEffect} from 'react';
+
+const PokemonForm = () => {
+
+    const[allPlayers, setAllPlayers] = useState([])
+
+    //  useEffect makes request one time
+    //  takes a ( callback function: ()=>{}, empty arr to run once [] || with variable to request everytime var is changed/updated [var1])
+    //  useEffect is saying, "run this code within the function, once (unless otherwise noted), when page is loaded"
+    useEffect(()=>{
+        // //  callbackFunction body
+        // fetch("https://pokeapi.co/api/v2/pokemon?limit=100&?")
+        //     .then(res => {
+        //         return res.json()
+        //     })
+        //     .then(res => {
+        //         console.log("*********")
+        //         console.log(res.results)
+        //          setAllPlayers(res.results) //   will send request on an infinite loop until computer crashes or you run out of requests from the API, unless inside useEffect callbackFunction
+        //     })
+        //     .catch(err => console.log("ERRRRRRROOOOOORRRRR", err))
+    }, [])
+    // console.log("Outside the fetch", res)
+
+    const getPlayers = ()=>{
+        fetch("https://pokeapi.co/api/v2/pokemon?limit=887&?")
+            .then(res => {
+                return res.json()
+            })
+            .then(res => {
+                console.log("*********")
+                console.log(res.results)
+                 setAllPlayers(res.results) //   will send request on an infinite loop until computer crashes or you run out of requests from the API, unless inside useEffect callbackFunction
+            })
+            .catch(err => console.log("ERRRRRRROOOOOORRRRR", err))
+    }
+
+
+    return (
+        <div>
+            <h1 className="text-center">Here Are The Players</h1>
+            <button onClick={getPlayers}>FETCH PLAYERS</button>
+            {
+            allPlayers.map((player, idx)=>{
+                return <div key={idx} className="card col-6 offset-md-4">
+                        <div  className="card-header">
+                            {player.name}
+                        </div>
+                </div>
+            })
+            }
+        </div>
+    );
+};
+
+
+export default PokemonForm;
